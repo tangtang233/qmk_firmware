f369b04f60ffc0568e96f1f6e640b80bc219f837
tangwei <toby911115@163.com>
2022-12-10 01:57:00 +0800

    :sparkle: new features - 添加了自己的键盘映射 - 在配列中添加了#SPLIT_USB_DETECT已解决右边键盘识别不了的问题

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/tangtang233/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/tangtang233/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,28 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 6
-
-// wiring of each half
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
-
-#define DIODE_DIRECTION COL2ROW
-
-// WS2812 RGB LED strip input and number of LEDs
-#define RGB_DI_PIN D3
-#define RGBLED_NUM 12
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
-
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000
+//#define EE_HANDS
